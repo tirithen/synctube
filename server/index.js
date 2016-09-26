1288dd00f9545318def5ad2e652a08023ed76b95
Fredrik S??derstr??m <fredrik.soderstrom@mojlighetsministeriet.se>
2016-09-26 20:05:15 +0200

    Channel endpoints

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-const server = require('express')();
+const httpServer = require('express')();
 
+const httpPort = process.env.PORT || 3000;
 const channels = new Map();
+const channelSecrets = new Map();
 
-server.get('/api/:channel', (request, response) => {
+function hasValidSecret(request, response, next) {
+	const channelId = request.params.channel;
+	const secret = request.body.secret;
+
+	if (secret && secret === channelSecrets.get(channelId)) {
+		next();
+	} else {
+		response.status(401).send('Invalid or missing secret');
+	}
+}
+
+httpServer.get('/api/:channel', (request, response) => {
 	const channel = channels.get(request.params.channel);
 
 	if (channel) {
@@ -12,6 +25,31 @@ server.get('/api/:channel', (request, response) => {
 	}
 });
 
-server.post('/api/:channel', () => {
+httpServer.post('/api/:channel', (request, response) => {
+	const channelId = request.params.channel;
+	const playlistId = request.body.playlistId;
+	const secret = request.body.secret;
+
+	const channel = channels.get(channelId);	
 	
+
+	if (channel) {
+		response.status(420).send('Channel already exists');
+	} else if (channelId && playlistId && secret) {
+		channels.set(channelId, { playlistId, id: channelId });
+		channelSecrets.set(channelId, secret);
+	}	
 });
+
+httpServer.delete('/api/:channel', hasValidSecret, (request, response) => {
+	const channel = channels.get(request.params.channel);
+
+	if (channel) {
+		channels.delete(channel.id);
+		response.send();
+	} else {
+		response.status(404).send('Not Found');
+	}
+});
+
+httpServer.listen(httpPort);
